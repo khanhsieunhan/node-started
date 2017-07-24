49fbbb908637ec5d5ec1727b5d2d12546f06890c
phamvankhanh <khanhpv@rikkeisoft.com>
2017-07-24 10:32:52 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
