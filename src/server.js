4e5f1033a7ad9faf3ad5270a0fec862c9841d308
phamvankhanh <khanhpv@rikkeisoft.com>
2017-07-24 14:17:16 +0700

    Add template engine

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = express()
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -10,4 +12,9 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+// use view engine ejs
+app.get('/about', function (req, res) {
+  res.render('about')
 })
