61916c5dfa00b98a9ae33ee2be2b57af648def34
Phạm Văn Khánh <khanhpoly@gmail.com>
2021-03-17 21:02:51 +0700

    Update package

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app = express()
-
+const express = require("express");
+const app = express();
+const path = require("path");
 // set the view engine to ejs
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log("Example app listening on port 3000!");
+});
 
 // use view engine ejs
-app.get('/about', function (req, res) {
-  res.render('about')
-})
+app.get("/about", function (req, res) {
+  res.render("about");
+});
