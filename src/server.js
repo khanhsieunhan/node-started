3e803ba4c70f9add18eaaa60ee2c96fee352cdbe
Phạm Văn Khánh <khanhpoly@gmail.com>
2021-03-17 21:04:00 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
